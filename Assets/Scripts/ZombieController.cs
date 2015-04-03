39091d40fd34911137f5b6f2cc4bf39e7fe0a52e
huangkaifeng <ro4tub@gmail.com>
2015-04-03 21:44:50 +0800

    init commit

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieController : MonoBehaviour {
+	public float moveSpeed;
+	public float turnSpeed;
+	private Vector3 moveDirection;
+	// Use this for initialization
+	void Start () {
+		moveDirection = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1
+		Vector3 currentPosition = transform.position;
+		// 2
+		if( Input.GetButton("Fire1") ) {
+			// 3
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			// 4
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+		}
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+
+		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+		transform.rotation = 
+			Quaternion.Slerp( transform.rotation, 
+			                 Quaternion.Euler( 0, 0, targetAngle ), 
+			                 turnSpeed * Time.deltaTime );
+
+		EnforceBounds ();
+	}
+	
+	private void EnforceBounds() {
+		Vector3 newPosition = transform.position;
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize;
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+
+		if (newPosition.x < xMin || newPosition.x > xMax) {
+			newPosition.x = Mathf.Clamp (newPosition.x, xMin, xMax);
+			moveDirection.x = -moveDirection.x;
+		}
+		transform.position = newPosition;
+	}
+}
